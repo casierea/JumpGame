787492f19d8a1ccff8b85337f3b1b90da7da2718
Casie <dancingmongoose@gmail.com>
2019-12-21 15:48:58 -0700

    mousevents

---
+++ gamejump/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+   public UnityEvent mouseUpEvent, mouseDownEvent;
+
+   private void OnMouseUp()
+   {
+       mouseUpEvent.Invoke();
+   }
+
+   private void OnMouseDown()
+   {
+       mouseDownEvent.Invoke();
+   }
+}
+
+
