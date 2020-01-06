8f6b6e5bbf1ae3056beda7904816b52c39d4cbfa
Unknown <dancingmongoose@gmail.com>
2020-01-05 19:41:53 -0700

    if practice. refresh

---
+++ gamejump/Assets/Scripts/IfPractice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfPractice : MonoBehaviour
+{
+    public float cowCount = 5;
+    public float mostCowCount = 10;
+    public float leastCowCount = 1;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            CowCountTest();
+
+        cowCount -= Time.deltaTime * 5f;
+    }
+
+    void CowCountTest()
+    {
+        if (cowCount > mostCowCount)
+        {
+            print("Where are the cows?");
+        }
+        else if (cowCount < leastCowCount)
+        {
+            print("All the cows");
+        }
+        else
+        {
+            print("Cows");
+        }
+    }
+}
